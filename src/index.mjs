6e18df03877f8e56017cbc37a7620afa63a206ad
Nicko Balboa <76141006+NBalboa@users.noreply.github.com>
2023-09-14 00:13:29 +0800

    cors added

--- src/index.mjs
+++ src/index.mjs
@@ -2,8 +2,14 @@
 import express from "express";
 import { routers } from "./middlewares/routers.mjs";
 import { testConnection } from "./configs/database.mjs";
+// const cors = require("cors");
+import cors from "cors";
+
 const app = express();
 const PORT = 3001;
+
+app.use(cors());
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
